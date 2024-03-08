0e016b3ea67ef8f8733d57a948d7c85dc611c7a4
NatanEasy <natan@easypoints.com.br>
2024-03-08 11:38:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './assets/styles/custom.scss';
+import './App.css';
+
+import MyRoutes from 'MyRoutes';
+
+const App = () => {
+  return <MyRoutes />;
+};
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,42 @@
+import './styles.css';
+
+import CarImage from 'assets/images/car-header.png';
+import ButtonIcon from 'components/ButtonIcon';
+
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-card-content">
+            <h5>O carro perfeito para você</h5>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
+          </div>
+          <div className="home-card-image">
+            <img src={CarImage} alt="Imagem do carro" />
+          </div>
+        </div>
+
+        <div className="home-card-2">
+          <div className="home-card-2-content">
+            <div className="home-card-2-button">
+              <Link to="/products">
+                <ButtonIcon />
+              </Link>
+            </div>
+            <div className="home-card-2-text">
+              <p>Comece agora a navegar</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
